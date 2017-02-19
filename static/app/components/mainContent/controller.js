cd41aaac34999894eb6517e1cc15a5f298c891dd
Aakash Mallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-02-19 17:49:36 +0530

    Added product details, reorganized and commented code now

--- static/app/components/mainContent/controller.js
+++ static/app/components/mainContent/controller.js
@@ -3,7 +3,16 @@ angular.module('mainContent')
 
     $scope.change=0;
     $scope.showDetails = false;
+    $scope.selected = {};
 
+    // Product details array
+    var productList = [
+        {abstract:"The Edusmart software kit is designed to give you the best expereince of management you have ever experienced. It is aimed at ease of use and exploit the full potential of computer technologies to give you the control of your coaching right at your finger tips",
+        pros:["Simple and Intutive UI","Plug in and forget","Online data security"],
+        price:"Price: $299/-"},
+    ];
+
+    // Animation control timeout
     $timeout(function() {
 		$scope.change=1;
 		$timeout(function(){
@@ -11,6 +20,7 @@ angular.module('mainContent')
 		},2000);
 	},1500);
 
+    // Navigations arrows
     $scope.goDown = function(){
         $scope.snapIndex+=1;
     };
@@ -19,7 +29,9 @@ angular.module('mainContent')
         $scope.snapIndex-=1;
     };
 
+    // Reder product details in description box
     $scope.renderDetails = function(product_index){
         $scope.showDetails = true;
+        $scope.selected = productList[product_index];
     };
 });
